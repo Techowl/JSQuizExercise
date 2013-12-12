740fea073189bdb7ff7e9948973461223d37510b
Daniel Kimbel <danielkimbel@Daniels-MacBook-Pro.local>
2013-12-11 17:36:49 -0800

    Refactored code and added 'play again' button

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   Sessions.init()
-  $('form#submission').on("submit", Sessions.onFormSubmit.bind(Sessions))
 })
 
 var Sessions = {
@@ -9,95 +8,125 @@ var Sessions = {
   numCorrect: 0,
   numIncorrect: 0,
   mostRecentResult: false,
+
   init: function() {
     Sessions.iD = Math.floor(Math.random() * 100000000000)
+    $('form#submission').on("submit", Sessions.displaySelectedQuiz.bind(Sessions))
+    $('#play-again-button').on("click", location.reload.bind(location))
     Sessions.displayQuizSelector()
   },
+
   displayQuizSelector: function() {
     $('.question-display').text("Which quiz would you like to take?")
-    $.ajax({
-      url: '/quizzes.json',
-      type: 'get'
-    }).done(function(data, textStatus, jqXHR) {
-      var quizzes = data.quizzes
-      for (var i=0;i<quizzes.length;i++) {
-        var $optionTemplate = $(".templates .choice").clone()
-          $optionTemplate.attr("value",quizzes[i].quiz_id)
-          $optionTemplate.text(quizzes[i].name)
-          $(".choice-display").append($optionTemplate)
-      }
-    })
+    $.get('/quizzes.json').done(this.displayQuizzes)
   },
-  onFormSubmit: function(e) {
+
+  displayQuizzes: function(data, textStatus, jqXHR) {
+    var quizzes = data.quizzes
+    for (var i in quizzes) {
+      Sessions.produceQuizOptionFromTemplate(quizzes[i])
+    }
+  },
+
+  produceQuizOptionFromTemplate: function(quiz) {
+    var $optionTemplate = $(".templates .choice").clone()
+    $optionTemplate.attr("value",quiz.quiz_id)
+    $optionTemplate.text(quiz.name)
+    $(".choice-display").append($optionTemplate)
+  },
+
+  displaySelectedQuiz: function(e) {
     e.preventDefault()
     Sessions.quizID = $('form#submission option:selected').val()
-    $('.choice-display').html('')
-    $('form#submission').unbind()
-    QuizQuestions.getNextQuestion()
-    $('.num-correct').removeClass('hidden')
-    $('.num-incorrect').removeClass('hidden')
-    $('form#submission').on("submit", AnswerSubmission.onFormSubmit.bind(AnswerSubmission))
+    QuizQuestions.init()
   }
 }
 
 var QuizQuestions = {
   iD: 0,
+
+  init: function() {
+    $('.choice-display').html('')
+    this.displayNextQuestion()
+    this.displayCorrectAndIncorrectAnswerFields()
+    this.changeSubmitButtonAction()
+  },
+
+  displayNextQuestion: function() {
+    $.get('/quizzes/' + Sessions.quizID + '/questions/next.json', {session_key: Sessions.iD})
+    .done(this.appendQuestionAndChoices.bind(this))
+    .error(this.displayFinalScreen.bind(this))
+  },
+
   appendQuestionAndChoices: function(data, textStatus, jqXHR){
     $(".question-display").text(data.question.question)
-
     QuizQuestions.iD = data.question.question_id
     var choices = data.question.choices
-    for (var i=0;i<choices.length;i++) {
-      var $optionTemplate = $(".templates .choice").clone()
-        $optionTemplate.attr("value",choices[i].choice_id)
-        $optionTemplate.text(choices[i].choice)
-        $(".choice-display").append($optionTemplate)
+    for (var i in choices) {
+      this.produceAnswerFromTemplate(choices[i])
     }
   },
+
+  produceAnswerFromTemplate: function(choice) {
+    var $optionTemplate = $(".templates .choice").clone()
+    $optionTemplate.attr("value",choice.choice_id)
+    $optionTemplate.text(choice.choice)
+    $(".choice-display").append($optionTemplate)
+  },
+
+  displayCorrectAndIncorrectAnswerFields: function() {
+    $('.num-correct').removeClass('hidden')
+    $('.num-incorrect').removeClass('hidden')
+  },
+
+  displayNumCorrectAndIncorrect: function() {
+    $('.num-correct').text("Correct answers: " + Sessions.numCorrect)
+    $('.num-incorrect').text("Incorrect answers: " + Sessions.numIncorrect)
+  },
+
+  changeSubmitButtonAction: function() {
+    $('form#submission').off("submit")
+    $('form#submission').on("submit", AnswerSubmission.submitUserAnswer.bind(AnswerSubmission))
+  },
+
   displayFinalScreen: function() {
     $('form#submission').html('')
     $('.question-display').text("Quiz completed!")
-  },
-  getNextQuestion: function() {
-    $.ajax({
-      url: '/quizzes/' + Sessions.quizID + '/questions/next.json',
-      type: 'get',
-      data: {session_key: Sessions.iD}
-    }).done(this.appendQuestionAndChoices.bind(this)).error(this.displayFinalScreen.bind(this))
+    $('#play-again-button').removeClass('hidden')
   }
 }
 
 var AnswerSubmission = {
   choiceID: 0,
-  onFormSubmit: function(e){
+
+  submitUserAnswer: function(e){
     e.preventDefault()
     AnswerSubmission.choiceID = $('form#submission option:selected').val()
-    $.ajax({
-      url: "/questions/" + QuizQuestions.iD + "/answers.json",
-      type: 'post',
-      data: {session_key: Sessions.iD, choice_id: AnswerSubmission.choiceID}
-    }).done(this.updateSessions.bind(this))
+    $.post("/questions/" + QuizQuestions.iD + "/answers.json", {session_key: Sessions.iD, choice_id: AnswerSubmission.choiceID})
+    .done(this.updateSessions.bind(this))
   },
+
   updateSessions: function(data, textStatus, jqXHR) {
-    $(".last-right").addClass("hidden")
-    $(".last-wrong").addClass("hidden")
+    $(".correctness-indicator").addClass("hidden")
     Sessions.numCorrect = data.status.num_correct
     Sessions.numIncorrect = data.status.num_incorrect
     Sessions.mostRecentResult = data.status.correct
     AnswerSubmission.updateAllDisplayedValues()
   },
+
   updateAllDisplayedValues: function() {
     $(".choice-display").html("")
+    this.displayAppropriateCorrectnessIndicator()
+    QuizQuestions.displayNumCorrectAndIncorrect()
+    QuizQuestions.displayNextQuestion()
+  },
+
+  displayAppropriateCorrectnessIndicator: function() {
     if (Sessions.mostRecentResult) {
       $(".last-right").toggleClass("hidden")
     } else {
       $(".last-wrong").toggleClass("hidden")
     }
-    $('.num-correct').text("Correct answers: " + Sessions.numCorrect)
-    $('.num-incorrect').text("Incorrect answers: " + Sessions.numIncorrect)
-    QuizQuestions.getNextQuestion()
-  },
-  listener: function() {
-    $('form#submission').on("submit", this.onFormSubmit.bind(this))
   }
 }
+
